3b55ddf553a2dabb00dd504f575f8b6390759e49
Echoic88 <caulfield.bd@outlook.com>
2019-09-24 09:41:51 +0000

    Tidier JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-
     //---------------------Define Variables --------------//
     let dimensions = []; //dimensions array - replace with user input
+    let frequencies = []; // for storing the frequency outputs
 
     //return the dimensions of drawing area for currently selected 
     let drawAreaHeight = $("#drawingArea").height();
     let drawAreaWidth = $("#drawingArea").width();
 
+
     //Remove selected class from dim-boxes when dimensions are being entered
     $(".input-box").click(function() {
         $(".dim-box").removeClass("dim-selected");
-    })
+    });
 
     //--------------------------Input Functions-----------//
-    //On submit the inputs will be assigned to the dimension boxes a
-    //nd [dimensions] array
+    //On submit the inputs will be assigned to the dimension boxes and [dimensions] array
     $("#submitBtn").on("click", addDimensions);
 
     function addDimensions() {
@@ -26,23 +26,24 @@ $(document).ready(function() {
         let yIn = parseFloat($("#yInput").val());
         let zIn = parseFloat($("#zInput").val());
 
-        if (isNaN(xIn) === true || 
-            isNaN(yIn) === true || 
-            isNaN(zIn) === true ||
+        if (isNaN(xIn) === true || isNaN(yIn) === true || isNaN(zIn) === true ||
             xIn <= 0 || yIn <= 0 || zIn <= 0) {
             invalidDimensionsError();
-        } else {
+        }
+        else {
             //Remove "d-none" class from svg elements"
             $(".line").removeClass("d-none");
-            //call function to fill the output display
-            dimensionOutputs(xIn, yIn, zIn);
+
+            dimensionOutputs(xIn, yIn, zIn); // call function to fill the output display on left
             drawRoom(xIn, yIn, zIn); // call function to draw the room
+
             $("#dimensionForm").trigger("reset");
         }
     }
 
     //Display error at entry of invalid dimensions 
     $(".input-box").blur(inputError);
+
     function inputError() {
         if (isNaN($(this).val()) || $(this).val() <= 0) {
             invalidDimensionsError();
@@ -59,6 +60,7 @@ $(document).ready(function() {
 
     //function to display dimension outputs
     function dimensionOutputs(x, y, z) {
+
         //Dimension outputs
         //clear dimensions array and push user inputs to dimensions array 
         dimensions.length = 0;
@@ -75,8 +77,9 @@ $(document).ready(function() {
         $("#freqWid").text(`Frequency: ${frequencies[1]}Hz`);
         $("#freqHei").text(`Frequency: ${frequencies[2]}Hz`);
 
+
         //Notes outputs
-        notes = dimensions.map(notesCalc);
+        let notes = dimensions.map(notesCalc);
         $("#noteLen").text(`Note: ${notes[0]}`);
         $("#noteWid").text(`Note: ${notes[1]}`);
         $("#noteHei").text(`Note: ${notes[2]}`);
@@ -84,27 +87,19 @@ $(document).ready(function() {
 
     //calculate frequency based on dimension
     function freqCalc(dimen) {
-        //speed of sound in air in meters per second (21 degrees celsius)
-        const speedOfSound = 344;
+        const speedOfSound = 344; //speed of sound in air in meters per second (21 degrees celsius)
         return Math.round(speedOfSound / dimen);
     }
 
-    // Determine the closest musicalnote based on 
-    //the room dimension (wavelength);
+    // Determine the closest musicalnote based on the room dimension (wavelength);
     function notesCalc(dimen) {
-        
-        //multiplier to be used for calculating frequencies/wavelengths 
-        //relative to the next frequency/wavelength
-        const wavelengthConstant = 1.059463;
-        
-        // base wavelength to be used to fill [wavelengths] array - 
-        //this corresponds to the first "A note" below the threshold of human hearing
-        const baseWavelength = 24.94545455; 
+
+        const wavelengthConstant = 1.059463; //multiplier to be used for calculating frequencies/wavelengths relative to the next frequency/wavelength 
+        const baseWavelength = 24.94545455; // base wavelength to be used to fill [wavelengths] array - this corresponds to the first "A note" below the threshold of human hearing  
         const noteNames = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"];
-        let frequencies = [];
-        let notes = [];
+
         let wavelengths = [baseWavelength];
-        //return the array of wavelengths linked to musical notes
+        //return the array of wavelengths corresponding to musical notes in range
         function generateWavelengths() {
             for (i = 1; i < 100; i++) {
                 wavelengths.push(wavelengths[i - 1] / wavelengthConstant);
@@ -112,20 +107,14 @@ $(document).ready(function() {
         }
         generateWavelengths();
 
-        //Generate array whoch subtracts the room dimension 
-        //from the wavelengths array
+        //Generate array whoch subtracts the room dimension from the wavelengths array
         let compareArray = wavelengths.map(n => n - dimen);
-        
-        // find first wavelength with a negative value and 
-        //last wavelength with positive value in compareArray
-        // whichever of these wavelengths is closest to zero corresponds 
-        //to the wavelngth of the closest musical note
+
+        // find first wavelength with a negative value and last wavelength with positive value in compareArray
+        // whichever of these wavelengths is closest to zero corresponds to the wavelngth of the closest musical note
         let firstNegative = compareArray.find(findNegative);
-        
-        //find the index of the first item in compareArray with a negative value 
-        let firstNegativeIndex = compareArray.indexOf(compareArray.find(findNegative));         
-        //get last item in compareArray with positive value
-        let lastPositiveIndex = firstNegativeIndex - 1; 
+        let firstNegativeIndex = compareArray.indexOf(compareArray.find(findNegative)); //find the index of the first item in compareArray with a negative value 
+        let lastPositiveIndex = firstNegativeIndex - 1; //get last item in compareArray with positive value
         let lastPositive = compareArray[lastPositiveIndex];
 
         function findNegative(n) {
@@ -134,14 +123,16 @@ $(document).ready(function() {
         let noteIndex;
         if (Math.abs(firstNegative) < Math.abs(lastPositive)) {
             noteIndex = firstNegativeIndex;
-        } else {
+        }
+        else {
             noteIndex = lastPositiveIndex;
         }
         let noteOutput;
         //Lookup up the correct note from noteNames array 
         if (noteIndex < 12) {
             noteOutput = noteNames[noteIndex];
-        } else {
+        }
+        else {
             noteOutput = noteNames[noteIndex % 12];
         }
         return noteOutput;
@@ -151,18 +142,23 @@ $(document).ready(function() {
     $("#showExplainer").click(function() {
         $("#explainerModal").removeClass("d-none");
     });
+
+
     $("#modalDismiss").click(function() {
         $("#explainerModal").addClass("d-none");
     });
+
+
     $("#explainerModal").click(function() {
         $(this).addClass("d-none");
-    })
+    });
 
     //------------------------------------- Display Functions -----------------//
     function drawRoom(xDim, yDim, zDim) {
-        //If the z-axis is equal to or larger than other x-axes the room 
-        //will bet oo large for the drawing area. This derives a scale to 
-        //to maximise diagram size based on room x-axis and y-axis
+        //If the z-axis is equal to or larger than other x-axes the room will be
+        //too large for the drawing area. This derives a scale to reduce the overall size
+        //derive the  to maximise diagram size based on room x-axis and y-axis
+
         //Remove the sine wave if it was previously on screen  
         $(".sine-wave").attr({
             d: ""
@@ -172,22 +168,23 @@ $(document).ready(function() {
         let scaleQ = xDim + yDim;
         if (Math.max(...dimensions) === zDim) {
             baseProportion = Math.round(drawAreaHeight / scaleQ) * 0.8;
-        } else {
+        }
+        else {
             baseProportion = Math.round(drawAreaWidth / scaleQ) * 0.8;
         }
-        
-        //x position of origin is based on length of x-axis (red)
-        let posQ = drawAreaWidth / (xDim + yDim) * xDim; 
+
+        let posQ = drawAreaWidth / (xDim + yDim) * xDim; //x position of origin is based on length of x-axis (red)
+
         //lengths Are dimension * baseProportion * value in [dimensions] array
         let xLength = Math.round(baseProportion * xDim);
         let yLength = Math.round(baseProportion * yDim);
         let zLength = Math.round(baseProportion * zDim);
+
         //set origin point - the point where the three main axes meet
         // Test with one third from left [x,y] and 90% from top
         let originX = Math.round(posQ);
         let originY = Math.round(drawAreaHeight) - zLength;
-        //angle x and y axes up by 10% draw area height
-        let angleUp = Math.round(drawAreaHeight * 0.1); 
+        let angleUp = Math.round(drawAreaHeight * 0.1); //angle x and y axes up by 10% draw area height
 
         //Draw main, coloured axes
         $("#xAxis").attr({
@@ -237,7 +234,8 @@ $(document).ready(function() {
             function frame() {
                 if (posX == originX - xLength) {
                     clearInterval(id);
-                } else {
+                }
+                else {
                     posX--;
                     $("#xAxis").attr("x2", posX);
                 }
@@ -252,7 +250,8 @@ $(document).ready(function() {
             function frame() {
                 if (pos == originX + yLength) {
                     clearInterval(id);
-                } else {
+                }
+                else {
                     pos++;
                     $("#yAxis").attr("x2", pos);
                 }
@@ -267,7 +266,8 @@ $(document).ready(function() {
             function frame() {
                 if (pos == originY + zLength) {
                     clearInterval(id);
-                } else {
+                }
+                else {
                     pos++;
                     $("#zAxis").attr("y2", pos);
                 }
@@ -277,20 +277,22 @@ $(document).ready(function() {
 
     // -------------------------- Axes Focus ---------------------//
     $(".dim-button").on("click", axisFocus);
+
+    function initialiseAxisFocus() {
+        $(".line").addClass("d-none"); //remove the main room drawing
+        $(".sine-wave").attr({
+            d: ""
+        }); //clear the sine wav from screen
+    }
+
     function axisFocus() {
         if (dimensions.length === 0) {
             invalidDimensionsError();
-        } else {
-            function initialiseAxisFocus() {
-                //remove the main room drawing
-                $(".line").addClass("d-none");
-                $(".sine-wave").attr({
-                    d: ""
-                }); //clear the sine wav from screen
-            }
+        }
+        else {
+            initialiseAxisFocus();
 
-            //calculate positions of nodes and antinodes 
-            //for each room mode
+            //calculate positions of nodes and antinodes for each room mode
             let nodes = dimensions.map(d => d / 2);
             let nodeValue;
             let axisName;
@@ -298,13 +300,15 @@ $(document).ready(function() {
 
             if ($(this).attr("id") === "lenBtn") {
                 nodeValue = nodes[0];
-                axisName = "length"
+                axisName = "length";
                 $(this).closest(".dim-box").addClass("dim-selected");
-            } else if ($(this).attr("id") === "widBtn") {
+            }
+            else if ($(this).attr("id") === "widBtn") {
                 nodeValue = nodes[1];
                 axisName = "width";
                 $(this).closest(".dim-box").addClass("dim-selected");
-            } else {
+            }
+            else {
                 nodeValue = nodes[2];
                 axisName = "height";
                 $(this).closest(".dim-box").addClass("dim-selected");
@@ -312,8 +316,7 @@ $(document).ready(function() {
 
             //display the axis information
             $("#axisInfo").removeClass("d-none");
-            $("#problemDistances").text(`Node at: ${nodeValue} 
-                                        meter/s for ${axisName} of room`)
+            $("#problemDistances").text(`Node at: ${nodeValue} meter/s for ${axisName} of room`)
 
             initialiseAxisFocus();
 
@@ -322,12 +325,8 @@ $(document).ready(function() {
             let yStart = drawAreaHeight * 0.1;
             let xEnd = drawAreaWidth * 0.9;
             let yEnd = drawAreaHeight * 0.8;
-            
-            //multiply by two so that is xStart changes length remains centered
-            let xSize = drawAreaWidth - (xStart * 2); 
-            
-            //multiply by two so that is xStart changes length remains centered
-            let ySize = drawAreaHeight - (yStart * 2); 
+            let xSize = drawAreaWidth - (xStart * 2); // multiply by two so that is xStart changes length remains centered
+            let ySize = drawAreaHeight - (yStart * 2); // multiply by two so that is xStart changes length remains centered
             let xHalf = xStart + xSize / 2;
             let yHalf = yStart + ySize / 2;
             let xControl = xSize / 4;
@@ -335,14 +334,17 @@ $(document).ready(function() {
 
             if ($(this).attr("id") === "lenBtn") {
                 xAxisFocus();
-            } else if ($(this).attr("id") === "widBtn") {
+            }
+            else if ($(this).attr("id") === "widBtn") {
                 yAxisFocus();
-            } else if ($(this).attr("id") === "heiBtn") {
+            }
+            else if ($(this).attr("id") === "heiBtn") {
                 zAxisFocus();
             }
 
             //View the x-axis only
             function xAxisFocus() {
+
                 //Draw x-axis on left side  of screen
                 $("#xAxis").attr({
                     x1: drawAreaWidth * 0.1,
@@ -419,9 +421,8 @@ $(document).ready(function() {
             }
         }
     }
+
     //---------------------------Audio Code  ---------------------//
-    // Audio Context code modified from 
-    //https://www.toptal.com/web/web-audio-api-tutorial
     //Establish the audio context
     let audioCtx = new(window.AudioContext);
     let sound = audioCtx.createOscillator();
@@ -440,15 +441,14 @@ $(document).ready(function() {
     //Change the display of the play buttons     
     function playBtnDisplay(actBtn) {
         if ($(`#${actBtn}`).text() === "Play") {
-            $(".play-button").removeClass("btn-danger")
-                                .addClass("btn-success");
+            $(".play-button").removeClass("btn-danger").addClass("btn-success");
             $(".play-button").text("Play");
-            $(`#${actBtn}`).addClass("btn-danger")
-                            .removeClass("btn-success").text("Stop");
+            $(`#${actBtn}`).addClass("btn-danger").removeClass("btn-success").text("Stop");
             startPlayback();
-        } else {
-            $(`#${actBtn}`).addClass("btn-success")
-                            .removeClass("btn-danger").text("Play");
+        }
+        else {
+            $(`#${actBtn}`).addClass("btn-success").removeClass("btn-danger").text("Play");
+
             stopPlayback();
         }
     }
